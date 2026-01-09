27073041c5bfbb8c10a00f799af348f7562bb43a
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-01-09 15:56:32 +0530

    feat: implement Place Bid Lambda function with bid validation and logging

---
+++ lambdas/place-bid/index.ts
@@ -0,0 +1,85 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { logger } from '../_shared/logger';
+import { PutCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { randomUUID } from 'crypto';
+
+const client = new DynamoDBClient({});
+const TABLE_NAME = process.env.AUCTIONS_TABLE!;
+
+export const handler = async (event: any, context: any) => {
+	const auctionId = event.pathParameters?.auctionId;
+	const body = JSON.parse(event.body || {});
+
+	const { bidderId, amount } = body;
+
+	if (!auctionId || !bidderId || !amount) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: 'Missing required fields' }),
+		};
+	}
+
+	const now = Math.floor(Date.now() / 1000);
+
+	logger('INFO', 'Place bid attempt', {
+		auctionId,
+		bidderId,
+		amount,
+		requestId: context.awsRequestId,
+	});
+
+	try {
+		await client.send(
+			new UpdateCommand({
+				TableName: TABLE_NAME,
+				Key: {
+					PK: `AUCTION#${auctionId}`,
+					SK: 'AUCTION',
+				},
+				UpdateExpression: `SET highestBidAmount = :amount, highestBidderId: :bidderId, updatedAt = :now`,
+				ConditionExpression: `status = :open AND highestBidAmount < :amount AND endsAt > :now`,
+				ExpressionAttributeValues: {
+					':amount': amount,
+					':bidderId': bidderId,
+					':open': 'OPEN',
+					':now': now,
+				},
+			})
+		);
+
+		const bidId = randomUUID();
+
+		await client.send(
+			new PutCommand({
+				TableName: TABLE_NAME,
+				Item: {
+					PK: `AUCTION#${auctionId}`,
+					SK: `BID#${now}#${bidId}`,
+					bidId,
+					bidderId,
+					amount,
+					createdAt: now,
+				},
+			})
+		);
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ message: 'Bid placed successfully.' }),
+		};
+	} catch (err: any) {
+		logger('WARN', 'Bid rejected', {
+			auctionId,
+			bidderId,
+			amount,
+			reason: err.name,
+		});
+
+		return {
+			statusCode: 409,
+			body: JSON.stringify({
+				message: 'Bid rejected. Amount too low or auction closed.',
+			}),
+		};
+	}
+};
