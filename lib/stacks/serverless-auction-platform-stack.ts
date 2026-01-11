ac7ddd47244209c8bff6818c145aa469c1dd50dc
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-01-11 21:41:14 +0530

    feat: implement ServerlessAuctionPlatformStack with API Gateway and Lambda integration

---
+++ lib/stacks/serverless-auction-platform-stack.ts
@@ -0,0 +1,35 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import { AuctionTable } from '../constructs/dynamodb-construct';
+import { AuctionLambdas } from '../constructs/lambda-construct';
+
+export class ServerlessAuctionPlatformStack extends Stack {
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
+
+		// Auction DynamoDB Table Defination or Construct
+		const auctionTable = new AuctionTable(this, 'AuctionTable');
+		const lambdas = new AuctionLambdas(this, 'AuctionLambdas', {
+			tableName: auctionTable.table.tableName,
+		});
+
+		const api = new apiGateway.RestApi(this, 'AuctionApi', { restApiName: 'Auction Service' });
+		const healthResources = api.root.addResource('health');
+		const auctionResources = api.root.addResource('auctions');
+
+		const auctionById = auctionResources.addResource('{auctionId}');
+		const bidResource = auctionById.addResource('bid');
+
+		// Api gateway config and healthcheck lambda implementation
+
+		lambdas.grantMetricPublishing();
+
+		healthResources.addMethod('GET', new apiGateway.LambdaIntegration(lambdas.healthCheckLambda));
+		auctionResources.addMethod('POST', new apiGateway.LambdaIntegration(lambdas.createAuctionLambda));
+		bidResource.addMethod('POST', new apiGateway.LambdaIntegration(lambdas.placeBidLambda));
+
+		auctionTable.table.grantWriteData(lambdas.createAuctionLambda);
+		auctionTable.table.grantWriteData(lambdas.placeBidLambda);
+	}
+}
