70b7505ec4da3536236375361ca78c8165dfe238
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-01-12 18:14:55 +0530

    feat: add AuctionScheduler construct for scheduling auction closures

---
+++ lib/constructs/event-bridge-construct.ts
@@ -0,0 +1,21 @@
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import { Construct } from 'constructs';
+import { Duration } from 'aws-cdk-lib';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+
+interface AuctionSchedulerProps {
+	processLambdaFunction: IFunction;
+}
+
+export class AuctionScheduler extends Construct {
+	constructor(scope: Construct, id: string, props: AuctionSchedulerProps) {
+		super(scope, id);
+
+		new events.Rule(this, 'AuctionCloseSchedule', {
+			description: 'Predocially closes expired auctions',
+			schedule: events.Schedule.rate(Duration.minutes(1)),
+			targets: [new targets.LambdaFunction(props.processLambdaFunction, { retryAttempts: 2 })],
+		});
+	}
+}
