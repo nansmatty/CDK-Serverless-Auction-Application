ea232d9e8603ffb2b2ae6713fd02b41aaf516727
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-01-18 17:50:17 +0530

    feat: add GenerateAuditFunction construct for creating an audit Lambda function

---
+++ lib/functions-construct/generate-audit-function-construct.ts
@@ -0,0 +1,36 @@
+import { Duration } from 'aws-cdk-lib';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { join } from 'path';
+
+interface GenerateAuditFunctionProps {
+	auditBucket: Bucket;
+	auctionTable: Table;
+}
+
+export class GenerateAuditFunction extends Construct {
+	public readonly generateAuditLambda: IFunction;
+
+	constructor(scope: Construct, id: string, props: GenerateAuditFunctionProps) {
+		super(scope, id);
+
+		this.generateAuditLambda = new NodejsFunction(this, 'GenerateAuditLambda', {
+			runtime: Runtime.NODEJS_22_X,
+			entry: join(__dirname, '..', '..', 'lambdas', 'generate-auction-audit', 'index.ts'),
+			handler: 'handler',
+			timeout: Duration.seconds(30),
+			memorySize: 512,
+			environment: {
+				AUDIT_BUCKET_NAME: props.auditBucket.bucketName,
+				AUCTIONS_TABLE: props.auctionTable.tableName,
+			},
+		});
+
+		// Permissions
+		props.auditBucket.grantWrite(this.generateAuditLambda);
+		props.auctionTable.grantReadData(this.generateAuditLambda);
+	}
+}
