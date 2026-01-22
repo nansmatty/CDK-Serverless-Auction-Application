5d7b72fc764b970d640e1399c508d463262e66c5
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-01-22 19:47:28 +0530

    feat: add userSignUpLambda to AuthLambdas construct with Node.js integration

--- lib/service-constructs/auth-lambda-construct.ts
+++ lib/service-constructs/auth-lambda-construct.ts
@@ -1,11 +1,25 @@
+import { IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
 interface AuthLambdasProps {
 	tableName: string;
 }
 
 export class AuthLambdas extends Construct {
+	public readonly userSignUpLambda: IFunction;
+
 	constructor(scope: Construct, id: string, props: AuthLambdasProps) {
 		super(scope, id);
+
+		this.userSignUpLambda = new NodejsFunction(this, 'SignupUser', {
+			runtime: Runtime.NODEJS_22_X,
+			entry: join(__dirname, '..', '..', 'auth-lambdas', 'signup', 'index.ts'),
+			handler: 'handler',
+			environment: {
+				AUTH_TABLE: props.tableName,
+			},
+		});
 	}
 }
