dbcabf5077ea38ad7fa841d214bf667e33f1f4f0
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-01-26 15:58:00 +0530

    fix: correct typo in JWT secret environment variable names

--- utils/jwt-utils-functions.ts
+++ utils/jwt-utils-functions.ts
@@ -2,10 +2,10 @@ import jwt from 'jsonwebtoken';
 
 export class JwtUtils {
 	static generateAccessToken(payload: object) {
-		return jwt.sign(payload, process.env.JWT_ACCESSS_SECRET!, { expiresIn: '15m' });
+		return jwt.sign(payload, process.env.JWT_ACCESS_SECRET!, { expiresIn: '15m' });
 	}
 
 	static generateRefreshToken(payload: object) {
-		return jwt.sign(payload, process.env.JWT_ACCESSS_SECRET!, { expiresIn: '7d' });
+		return jwt.sign(payload, process.env.JWT_REFRESH_SECRET!, { expiresIn: '7d' });
 	}
 }
