f91068ca11c968442403be60ce4764d1c57bb766
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-01-31 13:27:00 +0530

    feat: add NotificationQueue construct for auction closed notifications

---
+++ lib/service-constructs/notification-sqs-construct.ts
@@ -0,0 +1,23 @@
+import { Construct } from 'constructs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
+
+interface NotificationQueueProps {
+	environment: 'dev' | 'prod';
+}
+
+export class NotificationQueue extends Construct {
+	public readonly auctionClosedQueue: sqs.Queue;
+
+	constructor(scope: Construct, id: string, props: NotificationQueueProps) {
+		super(scope, id);
+
+		const isProd = props.environment === 'prod';
+
+		this.auctionClosedQueue = new sqs.Queue(this, 'AuctionClosedQueue', {
+			visibilityTimeout: Duration.seconds(30),
+			retentionPeriod: Duration.days(4),
+			removalPolicy: isProd ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
+		});
+	}
+}
